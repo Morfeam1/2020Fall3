e7a181d4569e8da9af018a3e8b6e562ff1abbdd7
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-11-11 15:32:53 -0500

    11/9/20 Update

--- Server/index.js
+++ Server/index.js
@@ -4,45 +4,45 @@ require('dotenv').config();
 
 const users = require('./controllers/users');
 
-console.log(process.env.BEST_CLASS);
-
-//Middleware
 const app = express()
 const port = process.env.PORT || 3000;
 
+console.log(process.env.BEST_CLASS);
 
-//Authentication / Authorization
+//  Middleware
 app.use(express.json());
-app.use("/",express.static(__dirname + '/../docs/'));
+app.use(express.static( __dirname + '/../docs/'))
 
+//  Authentication
+app.use(function(req, res, next) {
+  const arr = (req.headers.authorization || "").split(" ");
+  if(arr.length > 1 && arr[1] != null){
+      req.userId = +arr[1];
+  }
+  next();
+});
 
-//API
-app.get('/', (req, res, next) => {
-  res.send('Hello World, you requested ' + req.url)
+//  API
+app.get('/hello', (req, res, next) => {
+  res.send('Hello Hudson Valley! You requested ' + req.url)
 })
 
 app.use('/users', users);
 
 app.get('*', (req, res, next) => {
-  const filename = path.join( __dirname + '/../docs/index.html');
-  console.log(filename);
-  res.sendFile(filename);
+    const filename = path.join(__dirname, '/../docs/index.html');
+    console.log(filename);
+    res.sendFile( filename );
 })
 
-app.use(function(req,res,next){
-  const arr = (req.headers.authorization || "").split(" ");
-  if(arr.length > 1 && arr[1] != NULL){
-    req.usrId = +arr[1];
-  }
-  next(); 
-});
+app.use( (err, req, res, next) =>{
+    console.log(err);
+    res.status(err.status || 500).send( { message: err.message } )
+} )
 
-app.use( (err,req,res,next) =>{
-  console.log(err);
-  res.status(er.status ||500).send({message: err.message})
-});
 
-//Init
+
+//  Init
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
