5e9e4c9c2af99a00b14fda42d0f2f5680cbc0c23
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-11-24 15:22:22 -0500

    Additions from 11/19 - 11/23

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,43 +1,78 @@
 <template>
-  <div class = 'page'>
-      <h2 class = 'title is-2'>Feed Page</h2>
+  <div class="page">
+      <h2 class="title is-2">Feed Page</h2>
 
         <div class="columns">
-            <div class = "column is-two-thirds">
-                <Post v-for="(x, i) in posts" 
-                :key="i" 
-                :post="x" />  
+            <div class="column is-one-quarter" @mouseenter="error">
+                <Sidebar />
             </div>
-            <div class = "column is-one-third">
-                <SideBar />
+            <div class="column is-one-half">
+
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Add a picture
+                        </p>
+                    </header>
+                    
+                    <div class="card-content">
+
+                        <figure v-for="p in fbPics" :key="p.id" class="image is-64x64" @click.prevent="add(p)">
+                            <img :src="p.picture" alt="" />
+                        </figure>
+                    </div>
+                    
+                </div>
+
+                <Post v-for=" (x, i) in posts " 
+                      :key="i"
+                      :i="i"
+                      :post="x" /> 
+            </div>
+            <div class="column is-one-quarter" @mouseenter="error">
+                <Sidebar />
             </div>
         </div>
-    </div>
+  </div>
 </template>
 
 <script>
+import Sidebar from "@/components/Sidebar";
 import Post from "@/components/Post";
-import SideBar from "@/components/SideBar";
-import { getposts } from "@/models/feed";
+import { getPosts } from "@/models/feed";
 import session from "@/models/session";
-
 export default {
     data(){
-        return{
-            posts: []
+        return {
+            posts: [],
+            fbPics: []
         }
     },
     async created(){
-        this.posts = getposts();
+        this.posts = await getPosts();
+        FB.api("me/photos?fields=link,images,picture", x=>{
+            this.fbPics = x.data
+            console.log(x)
+        })
     },
     components: {
-        Post, SideBar
+        Sidebar, Post
+    },
+    methods: {
+        error(){
+            //session.addNotification('Something went wrong.', 'danger')
+        },
+        add(p){
+            this.posts.push({
+                URL: p.images[0].source
+            })
+        }
     }
 }
 </script>
 
 <style>
-    .card{
-        margin-top: 15px; 
+    .card {
+        margin-bottom: 30px ;
     }
 </style>
